65c5aa91b73e77b077709649a844b77eeb334383
cassy <cassycadmor@gmail.com>
2024-10-31 21:34:17 -0500

    Showed a screen that opened and closed from terminal.

--- src/project.py
+++ src/project.py
@@ -1,17 +1,18 @@
+import pygame
+
+
+pygame.init()
+
+
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+FPS = 60
+GRAVITY = 0.5
+
 def main():
-    ...
-
-
-class SomeClass():
-
-    def method_1():
-        ...
-
-    def method_2():
-        ...
-
-    def method_n():
-        ...
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    pygame.display.set_caption("Mario Clone")
+    pygame.quit()
 
 
 if __name__ == "__main__":
