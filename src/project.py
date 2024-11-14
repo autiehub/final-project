ec45f895b6d91ab42320c76cb1e1d74738f50e29
cassy <cassycadmor@gmail.com>
2024-11-14 02:33:25 -0600

    Deleted for loop in def main method.

--- src/project.py
+++ src/project.py
@@ -35,15 +35,13 @@ def main():
     screen = pygame.display.set_mode((WIDTH, HEIGHT))
     pygame.display.set_caption("Mario Clone")
     clock = pygame.time.Clock()
-    for coin in coins[:]:
-        coins.remove(coin)
-        score += 1
-        print(f"Score: {score}")
+    score = 0
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
     screen.blit(background_img, (0, 0))
-    for coin in coins:
-        screen.blit(coin.image, coin.rect)
-        pygame.display.flip()
-        clock.tick(FPS)
     pygame.quit()
 
 
