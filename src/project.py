b0abadf650f5b30bf0889d88e936955b55956fe8
cassy <cassycadmor@gmail.com>
2024-11-22 11:16:01 -0600

    Changed background.png and used pygame.transform.scale.convert() to scale background image.

--- src/project.py
+++ src/project.py
@@ -2,8 +2,8 @@ import pygame
 import random
 
 
-WIDTH = 800
-HEIGHT = 600
+WIDTH = 1920
+HEIGHT = 1080
 
 
 def main():
@@ -11,7 +11,8 @@ def main():
     clock = pygame.time.Clock()
     screen = pygame.display.set_mode((WIDTH, HEIGHT))
     pygame.display.set_caption("Mario Clone")
-    background_img = pygame.image.load('assets/background.png')
+    background_img = pygame.image.load('assets/background.png').convert()
+    background_img = pygame.transform.scale(background_img, (WIDTH, HEIGHT)).convert()
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -21,6 +22,7 @@ def main():
                 screen.blit(background_img, (x,y))
         pygame.display.flip()
         clock.tick(60)
+        pygame.quit()
 
 
 if __name__ == "__main__":
