08522f88d31ab1af80e625847bbd37015dabcf1f
cassy <cassycadmor@gmail.com>
2024-11-22 11:03:11 -0600

    Deleted background.png.

--- src/project.py
+++ src/project.py
@@ -2,46 +2,25 @@ import pygame
 import random
 
 
-pygame.init()
-
-
-WIDTH, HEIGHT = 800, 600
-FPS = 60
-
-
-WHITE = (255, 255, 255)
-
-
-background_img = pygame.image.load('assets/background.png')
-coin_img = pygame.image.load('assets/coin.png')
-
-
-coin_img = pygame.transform.scale(coin_img, (30, 30))
-
-
-
-
-class Coin():
-
-    def __init__(self):
-        self.image = coin_img
-        self.rect = self.image.get_rect
-        self.rect.x = random.randint(50, WIDTH - 50)
-        self.rect.y = random.randint(100, HEIGHT - 200)
+WIDTH = 800
+HEIGHT = 600
 
 
 def main():
+    pygame.init()
+    clock = pygame.time.Clock()
     screen = pygame.display.set_mode((WIDTH, HEIGHT))
     pygame.display.set_caption("Mario Clone")
-    clock = pygame.time.Clock()
-    score = 0
-    running = True
-    while running:
+    background_img = pygame.image.load('assets/background.png')
+    while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                running = False
-    screen.blit(background_img, (0, 0))
-    pygame.quit()
+                pygame.quit()
+        for x in range(0, WIDTH, background_img.get_width()):
+            for y in range(0, HEIGHT, background_img.get_height()):
+                screen.blit(background_img, (x,y))
+        pygame.display.flip()
+        clock.tick(60)
 
 
 if __name__ == "__main__":
