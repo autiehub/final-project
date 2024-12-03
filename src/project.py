ac2e9f11c3075ec90de2059f1cbddca8713526b6
cassy <cassycadmor@gmail.com>
2024-12-02 18:29:43 -0600

    Completed full code of Mario Clone.

--- src/project.py
+++ src/project.py
@@ -2,84 +2,100 @@ import pygame
 import random
 
 
-pygame.init()
+import pygame
+import random
 
 
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
+class Platform():
+
+    def __init__(self, x, y, width, height):
+        self.rect = pygame.Rect(x, y, width, height)
+
+    def draw(self, screen):
+        pygame.draw.rect(screen, (0, 128, 0), self.rect)  
 
 
-WHITE = (255, 255, 255)
-BLACK = (0, 0, 0)
-RED = (255, 0, 0)
-
-
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-pygame.display.set_caption("Mario Clone")
-
-
-class Player(pygame.sprite.Sprite):
+class Coin():
     
-    def __init__(self):
-        super().__init__()
-        self.image = pygame.Surface((50, 50))
-        self.image.fill(RED)
-        self.rect = self.image.get_rect()
-        self.rect.center = (SCREEN_WIDTH//2, SCREEN_HEIGHT//2)
-        self.speed_x = 0
-        self.speed_y = 0
+    def __init__(self, x, y):
+        self.image = pygame.Surface((20, 20))  
+        self.image.fill((255, 255, 0))  
+        self.rect = self.image.get_rect(topleft=(x, y)) 
 
-    def update(self):
-        self.speed_y += 1  
-        self.rect.x += self.speed_x
-        self.rect.y += self.speed_y
+    def draw(self, screen):
+        screen.blit(self.image, self.rect)
 
-        if self.rect.bottom > SCREEN_HEIGHT:
-            self.rect.bottom = SCREEN_HEIGHT
-            self.speed_y = 0
-
-        if self.rect.left < 0:
-            self.rect.left = 0
-        if self.rect.right > SCREEN_WIDTH:
-            self.rect.right = SCREEN_WIDTH
-
-    def jump(self):
-        self.speed_y = -15
-
-    def move_left(self):
-        self.speed_x = -5
-
-    def move_right(self):
-        self.speed_x = 5
-
-    def stop(self):
-        self.speed_x = 0
 
 def main():
-    player = Player()
-    all_sprites = pygame.sprite.Group()
-    all_sprites.add(player)
-    running = True
+    pygame.init()
+    WIDTH, HEIGHT = 800, 600
+    screen = pygame.display.set_mode((WIDTH, HEIGHT))
+    pygame.display.set_caption("Mario Clone")
     clock = pygame.time.Clock()
+    FPS = 60
+    WHITE = (255, 255, 255)
+    BLUE = (0, 0, 255)
+    player_size = (50, 50)
+    player_pos = [WIDTH // 2, HEIGHT - 100]
+    player_velocity = [0, 0]
+    GRAVITY = 1
+    JUMP_STRENGTH = -15
+    platforms = [
+        Platform(200, 500, 200, 20),
+        Platform(400, 400, 200, 20),
+        Platform(100, 300, 200, 20),
+    ]
+    coins = []
+    for platform in platforms:
+        x = random.randint(platform.rect.left + 20, platform.rect.right - 40)
+        y = platform.rect.top - 20 
+        coins.append(Coin(x, y))
+    collected_coins = 0
+    running = True
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-            elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_LEFT:
-                    player.move_left()
-                elif event.key == pygame.K_RIGHT:
-                    player.move_right()
-                elif event.key == pygame.K_UP:
-                    player.jump()
-            elif event.type == pygame.KEYUP:
-                if event.key in (pygame.K_LEFT, pygame.K_RIGHT):
-                    player.stop()
-        all_sprites.update()
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT]:
+            player_velocity[0] = -5
+        elif keys[pygame.K_RIGHT]:
+            player_velocity[0] = 5
+        else:
+            player_velocity[0] = 0
+        if keys[pygame.K_SPACE]:
+            if player_velocity[1] == 0:
+                player_velocity[1] = JUMP_STRENGTH
+        player_velocity[1] += GRAVITY
+        player_pos[0] += player_velocity[0]
+        player_pos[1] += player_velocity[1]
+        player_rect = pygame.Rect(player_pos[0], player_pos[1], *player_size)
+        on_platform = False
+        for platform in platforms:
+            if player_rect.colliderect(platform.rect):
+                if player_velocity[1] > 0 and player_rect.bottom >= platform.rect.top:
+                    player_pos[1] = platform.rect.top - player_size[1] 
+                    player_velocity[1] = 0 
+                    on_platform = True
+        if player_pos[1] > HEIGHT - player_size[1]:
+            player_pos[1] = HEIGHT - player_size[1]
+            player_velocity[1] = 0
+            on_platform = True
+        for coin in coins[:]:  
+            if player_rect.colliderect(coin.rect):  
+                coins.remove(coin)  
+                collected_coins += 1
         screen.fill(WHITE)
-        all_sprites.draw(screen)
+        pygame.draw.rect(screen, BLUE, (*player_pos, *player_size))
+        for platform in platforms:
+            platform.draw(screen)
+        for coin in coins:
+            coin.draw(screen)
+        font = pygame.font.SysFont(None, 36)
+        coin_text = font.render(f'Coins: {collected_coins}', True, (0, 0, 0))
+        screen.blit(coin_text, (10, 10))
         pygame.display.flip()
-        clock.tick(60)
+        clock.tick(FPS)
     pygame.quit()
 
 
